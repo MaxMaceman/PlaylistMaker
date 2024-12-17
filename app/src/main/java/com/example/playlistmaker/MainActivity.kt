c91852cb6fae5d371a17a01d4825a3ae209f28a8
AKIM <maceman.sa@gmail.com>
2024-12-18 01:37:21 +0300

    Revert "Теперь должно всё работать, по какой-то причине применение стиля руинило приложение на старте."
    This reverts commit f27a650113e94c6da1e177d863c1cb39472dc05e.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -6,30 +6,30 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val search = findViewById<Button>(R.id.search_btn)
-        val media = findViewById<Button>(R.id.media_btn)
-        val settings = findViewById<Button>(R.id.settings_btn)
+        val search_button = findViewById<Button>(R.id.search_btn)
+        val media_button = findViewById<Button>(R.id.media_btn)
+        val settings_button = findViewById<Button>(R.id.settings_btn)
 
-        search.setOnClickListener {
-            val settingsIntent = Intent(this, SearchActivity::class.java)
-            startActivity(settingsIntent)
+        media_button.setOnClickListener {
         }
 
-        media.setOnClickListener {
-            val settingsIntent = Intent(this, MediaActivity::class.java)
-            startActivity(settingsIntent)
-        }
-
-        settings.setOnClickListener {
+        settings_button.setOnClickListener {
             val settingsIntent = Intent(this, SettingsActivity::class.java)
             startActivity(settingsIntent)
         }
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+
+            override fun onClick(v: View?) {
+            }
+        }
+
+        search_button.setOnClickListener(buttonClickListener)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActiivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActiivity.kt
@@ -7,14 +7,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
-import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val return_button = findViewById<MaterialToolbar>(R.id.back)
+        val return_button = findViewById<ImageButton>(R.id.back)
 
         return_button.setOnClickListener {
             finish()
