b42f0f8a9bcea86179a99a98d515413610990652
AKIM <maceman.sa@gmail.com>
2024-12-20 15:26:32 +0300

    Жду разгромного ревью, меня жалеть не надо :) Единственное, я сильно отстаю по учебе, поэтому будет здорово если критичные замечания будут отдельно от того что я смогу поправить к следующей сдаче ДЗ.
    Если я могу как-то оптимизировать сой код, буду рад комментариям.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,78 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.button.MaterialButton
+import android.widget.EditText
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var clearButton: MaterialButton
+    private lateinit var inputEditText: EditText
+    private var searchQuery: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val returnButton = findViewById<MaterialToolbar>(R.id.searchToolbar)
+        clearButton = findViewById(R.id.search_close_button)
+        inputEditText = findViewById(R.id.editTextFrame)
+
+        returnButton.setOnClickListener {
+            finish()
         }
+
+
+        if (savedInstanceState != null) {
+            searchQuery = savedInstanceState.getString("search_query", "")
+            inputEditText.setText(searchQuery)
+        }
+
+        inputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchQuery = s.toString()
+                clearButton.isVisible = !s.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        clearButton.setOnClickListener {
+            inputEditText.text.clear()
+            clearButton.isVisible = false
+            searchQuery = ""
+            hideKeyboard()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("search_query", searchQuery)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchQuery = savedInstanceState.getString("search_query", "")
+        inputEditText.setText(searchQuery)
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+    }
+
+    private fun showKeyboard() {
+        inputEditText.requestFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(inputEditText, InputMethodManager.SHOW_IMPLICIT)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,21 @@
 package com.example.playlistmaker
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.net.Uri
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
+import android.os.Bundle
 import android.widget.Toast
+import android.widget.Button
+import android.content.Intent
+import android.widget.ImageButton
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+private lateinit var themeSwitch: SwitchMaterial
+private lateinit var sharedPreferences: SharedPreferences
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +24,67 @@ class SettingsActivity : AppCompatActivity() {
 
         val return_button = findViewById<MaterialToolbar>(R.id.back)
 
-        return_button.setOnClickListener {
-            finish()
+            return_button.setOnClickListener {
+                finish()
+            }
+
+        sharedPreferences = getPreferences(MODE_PRIVATE)
+        themeSwitch = findViewById(R.id.dayNightSwitcher)
+
+        themeSwitch.isChecked = sharedPreferences.getBoolean("isDarkTheme", false)
+
+        themeSwitch.setOnCheckedChangeListener { _, isChacked ->
+            if (isChacked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            with(sharedPreferences.edit()) {
+                putBoolean("isDarkTheme", isChacked)
+                apply()
+            }
+
         }
+
+        val share_button = findViewById<MaterialTextView>(R.id.share)
+
+            share_button.setOnClickListener {
+
+                val shareText = "https://practicum.yandex.ru/profile/android-developer/"
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, shareText)
+                    type = "text/plain"
+                }
+
+                val chooser = Intent.createChooser(sendIntent, "Поделиться с помощью:")
+                startActivity(chooser)
+        }
+
+        val support_button = findViewById<MaterialTextView>(R.id.support)
+
+        support_button.setOnClickListener {
+            val email = "maseman.sa@yandex.ru" // Убедитесь, что это правильный адрес
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val body = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // только для почты
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, body)
+            }
+            startActivity(emailIntent)
+        }
+
+        val agreement_button = findViewById<MaterialTextView>(R.id.user_agreement)
+            agreement_button.setOnClickListener {
+                val url = "https://yandex.ru/legal/practicum_offer/"
+                val agreementIntent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(url)
+                }
+                startActivity(agreementIntent)
+            }
     }
 }
