90753f722db7e8af17b0ac53184dbdbaae483bd7
AKIM <maceman.sa@gmail.com>
2024-12-23 10:02:11 +0300

    23.12.2024

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -10,11 +10,17 @@ import androidx.core.view.isVisible
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
 import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.TrackAdapter
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var clearButton: MaterialButton
     private lateinit var inputEditText: EditText
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var trackAdapter: TrackAdapter
+    private var tracksList: ArrayList<Track> = arrayListOf()
     private var searchQuery: String = ""
 
     companion object {
@@ -26,33 +32,42 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         val returnButton = findViewById<MaterialToolbar>(R.id.searchToolbar)
-            clearButton = findViewById(R.id.search_close_button)
-            inputEditText = findViewById(R.id.editTextFrame)
+        clearButton = findViewById(R.id.search_close_button)
+        inputEditText = findViewById(R.id.editTextFrame)
+        recyclerView = findViewById(R.id.search_tracksRecycle)
 
-                returnButton.setOnClickListener {
-                    finish()
-                }
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        trackAdapter = TrackAdapter(tracksList) { track ->
+        }
+        recyclerView.adapter = trackAdapter
 
-            searchQuery = savedInstanceState?.getString(SEARCH_QUERY_KEY, "") ?: ""
-            inputEditText.setText(searchQuery)
+        returnButton.setOnClickListener {
+            finish()
+        }
 
-            inputEditText.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        searchQuery = savedInstanceState?.getString(SEARCH_QUERY_KEY, "") ?: ""
+        inputEditText.setText(searchQuery)
 
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    searchQuery = s.toString()
-                    clearButton.isVisible = !s.isNullOrEmpty()
-                }
+        inputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-                override fun afterTextChanged(s: Editable?) {}
-            })
-
-            clearButton.setOnClickListener {
-                inputEditText.text.clear()
-                clearButton.isVisible = false
-                searchQuery = ""
-                hideKeyboard()
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchQuery = s.toString()
+                clearButton.isVisible = !s.isNullOrEmpty()
             }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        clearButton.setOnClickListener {
+            inputEditText.text.clear()
+            clearButton.isVisible = false
+            searchQuery = ""
+            hideKeyboard()
+        }
+
+        populateTrackList()
+        trackAdapter.notifyDataSetChanged()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -63,5 +78,12 @@ class SearchActivity : AppCompatActivity() {
     private fun hideKeyboard() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+    }
+
+    private fun populateTrackList() {
+        tracksList.add(Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
+        tracksList.add(Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"))
+        tracksList.add(Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"))
+        tracksList.add(Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"))
     }
 }
